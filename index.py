8e7060566646956ce0a79d0ecd38355758e63f96
Daniel Stahl <danstahl1138@gmail.com>
2021-12-08 13:06:23 -0600

    redid math

--- index.py
+++ index.py
@@ -1,74 +1,90 @@
-from math import pow
+from math import pow, exp
 import matplotlib.pyplot as plt
 
 
-def drift_l1(
-    eta: float,
-    l1_curr: float,
-    l2_curr: float,
-    l3_curr: float,
-    l2_drift: float,
-    l3_drift: float,
-) -> float:
-    return eta * l1_curr + eta * l2_curr - l2_drift + eta * l3_curr - l3_drift
+##### new functions
+def Y(K: float, A: float, L: float, V3: float, alpha: float) -> float:
+    return pow(K, alpha) * pow(A * L * V3, 1.0 - alpha)
 
 
-def drift_l2(
-    gamma0: float,
-    gamma1: float,
-    gamma2: float,
-    deviation_from_natural_interest_rate: float,  # i*-i
-    gdp_ratio: float,
-    l2_curr: float,
-) -> float:
-    return (
-        gamma0 + gamma1 * deviation_from_natural_interest_rate + gamma2 * gdp_ratio
-    ) * l2_curr
-
-
-# drift is constrained by fraction of output
-def drift_l3(v0: float, v1: float, gdp_ratio: float, l3_curr: float, y: float) -> float:
-    den = y * gdp_ratio
-    return (v0 + v1 * gdp_ratio) * l3_curr * (1.0 - l3_curr / den) if den > 0.0 else 0.0
-
-
-def drift_a(l2_curr: float, beta: float) -> float:
-    return beta * l2_curr
-
-
-def y(k_curr: float, a_curr: float, l1_curr: float, alpha: float) -> float:
-    return pow(k_curr, alpha) * pow(a_curr * l1_curr, 1.0 - alpha)
-
-
-def drift_k(
-    k_curr: float,
-    a_curr: float,
-    l1_curr: float,
-    l2_curr: float,
+def drift_K(
+    K: float,
+    A: float,
+    L: float,
+    V1: float,
+    V3: float,
     s: float,
     delta: float,
     alpha: float,
 ) -> float:
-    return s * y(k_curr, a_curr, l1_curr, alpha) - l2_curr - delta * k_curr
+    return s * Y(K, A, L, V3, alpha) - L * V1 - delta * K
 
 
-def drift_rate(a: float, natural_rate: float, rate_curr: float) -> float:
-    return a * (natural_rate - rate_curr)
+def drift_A(beta: float, L: float, V1: float) -> float:
+    return beta * L * V1
+
+
+def l1(
+    gamma0: float,
+    gamma1: float,
+    gamma2: float,
+    current_rate_deviation: float,
+    current_govt: float,
+) -> float:
+    return exp(gamma0 + gamma1 * current_rate_deviation + gamma2 * current_govt)
+
+
+def l2(psi0: float, psi1: float, current_govt: float) -> float:
+    return exp(psi0 + psi1 * current_govt)
+
+
+def get_current_rate_deviation(
+    long_run_rate: float, shock_rate: float, a: float, t: float
+) -> float:
+    return (long_run_rate - shock_rate) * exp(-a * t)
+
+
+def get_current_govt(
+    long_run_govt: float, original_govt: float, b: float, t: float
+) -> float:
+    exp_b = exp(-b * t)
+    return original_govt * exp_b + long_run_govt * (1 - exp_b)
+
+
+# proportion of labor for technology
+def V1(l1: float, l2: float) -> float:
+    return l1 / (1.0 + l1 + l2)
+
+
+# proportion of labor for administration
+def V2(l1: float, l2: float) -> float:
+    return l2 / (1.0 + l1 + l2)
+
+
+# proportion of "traditional" labor
+def V3(l1: float, l2: float) -> float:
+    return 1.0 / (1.0 + l1 + l2)
+
+
+def L(eta: float, init_L: float, t: float) -> float:
+    return init_L * exp(eta * t)
+
+
+####
 
 
 class Economy:
-    def __init__(self, y, k, a, shock_rate, l1, l2, l3):
-        total_l = l1 + l2 + l3
+    def __init__(self, y, k, a, l, v1, v2, v3):
+
         self.y = [y]
         self.k = [k]
         self.a = [a]
-        self.l1 = [l1]
-        self.l2 = [l2]
-        self.l3 = [l3]
-        self.y_per_labor = [y / total_l]
+        self.l = [l]
+        self.v1 = [v1]
+        self.v2 = [v2]
+        self.v3 = [v3]
+        self.y_per_labor = [y / l]
         self.t = [0]
-        self.total_l = [total_l]
-        self.rate = [shock_rate]
 
 
 class EvolveEconomy:
@@ -78,110 +94,143 @@ class EvolveEconomy:
         gamma0,
         gamma1,
         gamma2,
-        v0,
-        v1,
+        psi0,
+        psi1,
         a,
+        b,
         beta,
         alpha,
         delta,
         s,
-        l1_init,
-        l2_init,
-        l3_init,
+        l_init,
         a_init,
         k_init,
+        natural_rate,
+        shock_rate,
+        original_govt,
+        long_run_govt,
     ):
+        self.natural_rate = natural_rate
+        self.shock_rate = shock_rate
+        self.original_govt = original_govt
+        self.long_run_govt = long_run_govt
+        curr_gov = get_current_govt(long_run_govt, original_govt, b, 0)
+        init_l1 = l1(
+            gamma0,
+            gamma1,
+            gamma2,
+            get_current_rate_deviation(natural_rate, shock_rate, a, 0),
+            curr_gov,
+        )
+        init_l2 = l2(psi0, psi1, curr_gov)
+
+        init_V1 = V1(init_l1, init_l2)
+        init_V2 = V2(init_l1, init_l2)
+        init_V3 = V3(init_l1, init_l2)
+        # V3(init_l1*
+
         self.eta = eta
         self.gamma0 = gamma0
         self.gamma1 = gamma1
         self.gamma2 = gamma2
-        self.v0 = v0
-        self.v1 = v1
+        self.psi0 = psi0
+        self.psi1 = psi1
         self.a = a
+        self.b = b
         self.beta = beta
         self.alpha = alpha
         self.delta = delta
         self.s = s
-        self.l1_init = l1_init
-        self.l2_init = l2_init
-        self.l3_init = l3_init
+        self.l_init = l_init
         self.a_init = a_init
         self.k_init = k_init
-        self.y_init = y(k_init, a_init, l1_init, alpha)
+        self.v1_init = init_V1
+        self.v2_init = init_V2
+        self.v3_init = init_V3
+        self.y_init = Y(k_init, a_init, l_init, init_V3, alpha)
 
     def execute_period(
         self,
         dt: float,
-        natural_rate: float,
-        # deviation_from_natural_interest_rate: float,
-        gdp_ratio: float,
+        # natural_rate: float,
+        # shock_rate: float,
+        # gdp_ratio: float,
         econ: Economy,
     ):
-        l2_drift = drift_l2(
+        t = econ.t[-1] + dt
+        interest_rate_deviation = get_current_rate_deviation(
+            self.natural_rate, self.shock_rate, self.a, t
+        )
+        government_rate = get_current_govt(
+            self.long_run_govt, self.original_govt, self.b, t
+        )
+
+        curr_l1 = l1(
             self.gamma0,
             self.gamma1,
             self.gamma2,
-            natural_rate - econ.rate[-1],
-            gdp_ratio,
-            econ.l2[-1],
+            interest_rate_deviation,
+            government_rate,
         )
-        l3_drift = drift_l3(self.v0, self.v1, gdp_ratio, econ.l3[-1], econ.y[-1])
-        l1_drift = drift_l1(
-            self.eta, econ.l1[-1], econ.l2[-1], econ.l3[-1], l2_drift, l3_drift
-        )
-        rate_drift = drift_rate(self.a, natural_rate, econ.rate[-1])
-        k_drift = drift_k(
-            econ.k[-1],
-            econ.a[-1],
-            econ.l1[-1],
-            econ.l2[-1],
-            self.s,
-            self.delta,
-            self.alpha,
-        )
-        a_drift = drift_a(econ.l2[-1], self.beta)
-        curr_l1 = max(l1_drift * dt + econ.l1[-1], 0)
-        curr_l2 = max(l2_drift * dt + econ.l2[-1], 0)
-        curr_l3 = max(l3_drift * dt + econ.l3[-1], 0)
+        curr_l2 = l2(self.psi0, self.psi1, government_rate)
 
-        curr_k = max(econ.k[-1] * k_drift * dt + econ.k[-1], 0)
-        curr_a = a_drift * dt + econ.a[-1]
-        curr_y = y(curr_k, curr_a, curr_l1, self.alpha)
-        curr_l = curr_l1 + curr_l2 + curr_l3
-        curr_rate = econ.rate[-1] + rate_drift * dt
-        econ.l1.append(curr_l1)
-        econ.l2.append(curr_l2)
-        econ.l3.append(curr_l3)
-        econ.k.append(curr_k)
+        curr_V1 = V1(curr_l1, curr_l2)
+
+        curr_V2 = V2(curr_l1, curr_l2)
+
+        curr_V3 = V3(curr_l1, curr_l2)
+
+        curr_L = L(self.eta, self.l_init, t)
+
+        curr_a = econ.a[-1] + drift_A(self.beta, econ.l[-1], econ.v1[-1]) * dt
+
+        curr_k = (
+            econ.k[-1]
+            + drift_K(
+                econ.k[-1],
+                econ.a[-1],
+                econ.l[-1],
+                econ.v1[-1],
+                econ.v3[-1],
+                self.s,
+                self.delta,
+                self.alpha,
+            )
+            * dt
+        )
+
+        econ.l.append(curr_L)
+        econ.v1.append(curr_V1)
+        econ.v2.append(curr_V2)
+        econ.v3.append(curr_V3)
         econ.a.append(curr_a)
+        curr_y = Y(curr_k, curr_a, curr_L, curr_V3, self.alpha)
         econ.y.append(curr_y)
-        econ.rate.append(curr_rate)
-
-        econ.y_per_labor.append(curr_y / curr_l)
-        econ.total_l.append(curr_l)
+        # econ.rate.append(interest_rate_deviation+)
+        econ.y_per_labor.append(curr_y / curr_L)
         econ.t.append(econ.t[-1] + dt)
 
     def simulate(
         self,
         # deviation_from_natural_interest_rate: float,
-        natural_rate: float,
-        shock_rate: float,
-        gdp_ratio: float,
+        # natural_rate: float,
+        # shock_rate: float,
+        # gdp_ratio: float,
         t: float,
         n: int,
-    ) -> Economy:
+    ) -> Economy:  # y, k, a, l, v1, v2, v3, shock_rate, govt
         econ = Economy(
             y=self.y_init,
             k=self.k_init,
             a=self.a_init,
-            shock_rate=shock_rate,
-            l1=self.l1_init,
-            l2=self.l2_init,
-            l3=self.l3_init,
+            l=self.l_init,
+            v1=self.v1_init,
+            v2=self.v2_init,
+            v3=self.v3_init,
         )
         dt = t / (n)
         for i in range(n):
-            self.execute_period(dt, natural_rate, gdp_ratio, econ)
+            self.execute_period(dt, econ)
         return econ
 
 
@@ -190,58 +239,48 @@ if __name__ == "__main__":
         {
             "eta": 0.02,
             "gamma0": 0.004,
-            "gamma1": 1.5,
-            "gamma2": 0.005,  # small amount of spending on R&D
-            "v0": 0.001,  # Low rate of bureacracy with no govt spending
-            "v1": 0.05,
+            "gamma1": 3.0,
+            "gamma2": 0.05,  # small amount of spending on R&D
+            "psi0": 0.001,  # Low rate of bureacracy with no govt spending
+            "psi1": 2.5,
             "a": 0.1,
+            "b": 0.1,
             "beta": 0.03,
             "alpha": 0.4,
             "delta": 0.1,
             "s": 0.3,
-            "l1_init": 3.0,
-            "l2_init": 1.0,
-            "l3_init": 1.0,
+            "l_init": 5.0,
             "a_init": 2.0,
             "k_init": 10.0,
-        },
-        {
-            "eta": 0.02,
-            "gamma0": 0.02,  # large enough to cause L2 to explode relative to l1 and l3
-            "gamma1": 1.5,  # this should increase L2...right?
-            "gamma2": 0.005,  # small amount of spending on R&D
-            "v0": 0.001,  # Low rate of bureacracy with no govt spending
-            "v1": 0.05,
-            "a": 0.1,
-            "beta": 0.03,
-            "alpha": 0.4,
-            "delta": 0.1,
-            "s": 0.3,
-            "l1_init": 3.0,
-            "l2_init": 1.0,
-            "l3_init": 1.0,
-            "a_init": 2.0,
-            "k_init": 10.0,
-        },
+            # "natural_rate": 0.08,
+            # "shock_rate": 0.02,
+            # "original_govt": 0.15,
+            # "long_run_govt": 0.25
+        }
     ]
     example_inputs = [
-        (0.08, 0.05, 0.15),
-        (0.08, 0.02, 0.15),
-        (0.08, 0.05, 0.0),
-        (0.08, 0.1, 0.0),
-        (0.08, 0.08, 0.15),
-        (0.08, 0.15, 0.15),
-        (0.08, 0.05, 0.8),
+        (0.08, 0.05, 0.15, 0.15),
+        (0.08, 0.02, 0.15, 0.15),
+        (0.08, 0.05, 0.0, 0.15),
+        (0.08, 0.1, 0.0, 0.15),
+        (0.08, 0.08, 0.15, 0.15),
+        (0.08, 0.15, 0.15, 0.15),
+        (0.08, 0.05, 0.15, 0.8),
     ]
     t = 100
     n = 100000
     for index, economy in enumerate(economy_parameters):
-        econ = EvolveEconomy(**economy)
-        for sub_index, (natural_rate, rate_shock, ratio) in enumerate(example_inputs):
+
+        # econ = EvolveEconomy(**economy)
+        for sub_index, (natural_rate, rate_shock, original_govt, new_govt) in enumerate(
+            example_inputs
+        ):
+            economy["natural_rate"] = natural_rate
+            economy["shock_rate"] = rate_shock
+            economy["original_govt"] = original_govt
+            economy["long_run_govt"] = new_govt
+            econ = EvolveEconomy(**economy)
             sim = econ.simulate(
-                natural_rate=natural_rate,
-                shock_rate=rate_shock,
-                gdp_ratio=ratio,
                 t=t,
                 n=n,
             )
@@ -251,26 +290,28 @@ if __name__ == "__main__":
             plt.plot(
                 sim.t,
                 sim.y_per_labor,
-                label=f"rate shock={rate_shock}, gdp_ratio={ratio}",
+                label=f"rate shock={rate_shock}, initial govt={original_govt}, end govt={new_govt}",
             )
             plt.figure(f"econ_{index}_run_{sub_index}_labor")
             plt.plot(
                 sim.t,
-                sim.l1,
-                label=f"L1",
+                sim.v1,
+                label=f"V1",
             )
             plt.plot(
                 sim.t,
-                sim.l2,
-                label=f"L2",
+                sim.v2,
+                label=f"V2",
             )
             plt.plot(
                 sim.t,
-                sim.l3,
-                label=f"L3",
+                sim.v3,
+                label=f"V3",
             )
             plt.legend(loc="upper left")
-            plt.title(f"Labor for rate shock={rate_shock}, gdp_ratio={ratio}")
+            plt.title(
+                f"Labor proportions for rate shock={rate_shock}, initial govt={original_govt}, end govt={new_govt}"
+            )
             plt.savefig(f"images/econ_{index}_run_{sub_index}_labor.eps", format="eps")
         plt.figure(f"econ_{index}_run_main")
         plt.title(f"Output per capita")
